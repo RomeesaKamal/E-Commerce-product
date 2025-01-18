5a290c61b46433eb063fd6b5055081fd8af9a485
RomeesaKamal <kamalromeesa@gmail.com>
2025-01-18 20:25:42 +0500

    adding function

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,22 +2,54 @@ import React from 'react';
 import thumbnail from '../images/image-product-1-thumbnail.jpg';
 import iconDelete from '../images/icon-delete.svg';
 
-export default function Cart() {
-    const text = 'Autumn Limited Edition Sneakers'
-  return (
-   <article className='bg-white rounded-2xl shadow-2xl p-8 absolute right-8 top-32 left-8 z-[1000] lg:w-96 lg:left-auto lg:top-20'>
-    <h2 className='border-b border-slate-400 font-bold pb-2 mb-8'>Cart</h2>
-    <div className='flex items-center justify-between '>
-        <img src= {thumbnail} alt="" className='rounded-lg w-14' />
-        <ul>
-            <li className='text-slate-600 text-sm'>{`${text.substring(0, 23)}...`}</li>
-            <li className='text-slate-600 text-sm'>$125.00 x 3 <span className='font-bold text-slate-900'>$375.00</span></li>
-        </ul>
-        <button>
-            <img src={iconDelete} alt="" />
-        </button>
-    </div>
-    <button className="bg-orange-500 text-white font-blod rounded-lg mt-5  py-2 px-4 w-full  hover:bg-orange-600 transition-all duration-200">Checkout</button>
-   </article>
-  )
+export default function Cart({ numberOfItems, pricePerItem, setAmount }) {
+	const text = 'Autumn Limited Edition Sneakers';
+	if (numberOfItems === 0)
+		return (
+			<article className='bg-white rounded-2xl shadow-2xl p-8 absolute right-8 top-32 left-8 z-[1000] lg:w-96 lg:left-auto lg:top-20'>
+				<h2 className='border-b border-slate-400 font-bold pb-2 mb-8'>Cart</h2>
+				<p className='text-slate-600 text-sm'>Your cart is empty</p>
+			</article>
+		);
+	return (
+		<article className='bg-white rounded-2xl shadow-2xl p-8 absolute right-8 top-32 left-8 z-[1000] lg:w-96 lg:left-auto lg:top-20'>
+			<h2 className='border-b border-slate-400 font-bold pb-2 mb-8'>Cart</h2>
+			<div className='flex items-center justify-between '>
+				<img
+					src={thumbnail}
+					alt=''
+					className='rounded-lg w-14'
+				/>
+				<ul>
+					<li className='text-slate-600 text-sm'>{`${text.substring(
+						0,
+						23,
+					)}...`}</li>
+					<li className='text-slate-600 text-sm'>
+						{numberOfItems > 1
+							? `${numberOfItems} items`
+							: `${numberOfItems} item`}{' '}
+						<span className='font-bold text-slate-900'>
+							{numberOfItems > 1
+								? `$${numberOfItems * pricePerItem}.00`
+								: `$${numberOfItems * pricePerItem}.00`}
+						</span>
+					</li>
+				</ul>
+				<button
+					className='cursor-pointer'
+					onClick={() => setAmount(numberOfItems - 1)}
+				>
+					<img
+						src={iconDelete}
+						alt=''
+					/>
+				</button>
+			</div>
+			<button className='bg-orange-500 text-white font-blod rounded-lg mt-5  py-2 px-4 w-full  hover:bg-orange-600 transition-all duration-200'>
+				Checkout
+			</button>
+		</article>
+	);
 }
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,59 +1,109 @@
-import logo from "../images/logo.svg";
-import avatar from "../images/image-avatar.png";
-import menu from "../images/icon-menu.svg";
-import { AiOutlineShoppingCart } from "react-icons/ai";
-import { useState } from "react";
-import close from "../images/icon-close.svg";
-import Cart from "./Cart";
+import logo from '../images/logo.svg';
+import avatar from '../images/image-avatar.png';
+import menu from '../images/icon-menu.svg';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { useState, useRef, useEffect } from 'react';
+import close from '../images/icon-close.svg';
+import Cart from './Cart';
 
-export default function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [cartIsOpen, setCartIsOpen] = useState(false)
-  return (
-    <>
-      <header className=" relative flex items-center justify-between p-8 border-b border-slate-400 max-w-7xl mx-auto">
-        <div className="flex items-center justify-start gap-4">
-          <ul className="flex items-center justify-center gap-4">
-            {!isOpen && (
-              <li onClick={() => setIsOpen(true)} className="lg:hidden">
-                <img src={menu} alt="" className="cursor-pointer" />
-              </li>
-            )}
-            {isOpen && (
-              <li onClick={() => setIsOpen(false)} className="lg:hidden close">
-                <img src={close} alt="" className="cursor-pointer w-4" />
-              </li>
-            )}
-            <li>
-              <img src={logo} alt="" />
-            </li>
-          </ul>
-          <nav className={isOpen && "open"}>
-            <ul className="">
-              <li>Collections</li>
-              <li>Men</li>
-              <li>Women</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-        </div>
-        <div>
-          <ul className="flex items-center justify-start gap-4">
-            <li>
-              <button onClick={() => setCartIsOpen(!cartIsOpen)}>
-                <AiOutlineShoppingCart className="text-2xl text-slate-600" />
-              </button>
-            </li>
-            <li>
-             {cartIsOpen &&  <Cart/>}
-            </li>
-            <li>
-              <img src={avatar} className="w-12" alt="" />
-            </li>
-          </ul>
-        </div>
-      </header>
-    </>
-  );
+export default function Header({ numberOfItems, pricePerItem, setAmount }) {
+	const [isOpen, setIsOpen] = useState(false);
+	const [cartIsOpen, setCartIsOpen] = useState(false);
+	const cartRef = useRef(null);
+
+	const handleClickOutside = (event) => {
+		if (cartRef.current && !cartRef.current.contains(event.target)) {
+			setCartIsOpen(false);
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, []);
+
+	return (
+		<>
+			<header className='relative flex items-center justify-between p-8 border-b border-slate-400 max-w-7xl mx-auto'>
+				<div className='flex items-center justify-start gap-4'>
+					<ul className='flex items-center justify-center gap-4'>
+						{!isOpen && (
+							<li
+								onClick={() => setIsOpen(true)}
+								className='lg:hidden'
+							>
+								<img
+									src={menu}
+									alt=''
+									className='cursor-pointer'
+								/>
+							</li>
+						)}
+						{isOpen && (
+							<li
+								onClick={() => setIsOpen(false)}
+								className='lg:hidden close'
+							>
+								<img
+									src={close}
+									alt=''
+									className='cursor-pointer w-4'
+								/>
+							</li>
+						)}
+						<li>
+							<img
+								src={logo}
+								alt=''
+							/>
+						</li>
+					</ul>
+					<nav className={isOpen && 'open'}>
+						<ul className=''>
+							<li>Collections</li>
+							<li>Men</li>
+							<li>Women</li>
+							<li>About</li>
+							<li>Contact</li>
+						</ul>
+					</nav>
+				</div>
+				<div>
+					<ul className='relative flex items-center justify-start gap-4'>
+						<li className='relative'>
+							<button
+								onClick={() => setCartIsOpen(!cartIsOpen)}
+								className='relative'
+							>
+								<AiOutlineShoppingCart className='relative text-2xl text-slate-600' />
+								{!numberOfItems ? null : (
+									<span className='absolute bg-orange-500 text-white font-bold rounded-full w-6 h-5 flex text-xs items-center justify-center top-0 right-0 transform translate-x-1/2 -translate-y-1/2'>
+										{numberOfItems}
+									</span>
+								)}
+							</button>
+						</li>
+						<li ref={cartRef}>
+							{cartIsOpen && (
+								<Cart
+									numberOfItems={numberOfItems}
+									pricePerItem={pricePerItem}
+									setAmount={setAmount}
+								/>
+							)}
+						</li>
+						<li>
+							<img
+								src={avatar}
+								className='w-12'
+								alt=''
+							/>
+						</li>
+					</ul>
+				</div>
+			</header>
+		</>
+	);
 }
